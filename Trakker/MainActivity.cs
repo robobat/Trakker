109682690d32389912436f2fcba8802bf69a668f
robobat <robobat91@gmail.com>
2015-10-24 13:04:13 -0400

    Installed Support Libraries and working

--- Trakker/MainActivity.cs
+++ Trakker/MainActivity.cs
@@ -6,11 +6,13 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
 
 namespace Trakker
 {
 	[Activity (Label = "Trakker", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity
+	public class MainActivity : AppCompatActivity
 	{
 		int count = 1;
 
@@ -20,6 +22,11 @@ namespace Trakker
 
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
+			var toolbar = FindViewById<Toolbar> (Resource.Id.toolbar);
+			//Toolbar will now take on default actionbar characteristics
+			SetSupportActionBar (toolbar);
+			SupportActionBar.SetHomeAsUpIndicator (Resource.Drawable.ic_menu);
+			SupportActionBar.SetDisplayHomeAsUpEnabled (true);
 
 			// Get our button from the layout resource,
 			// and attach an event to it
